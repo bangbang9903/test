82ea25824760209348f8b3bca4d05e0ecf73d624
bangbang9903 <162521572+bangbang9903@users.noreply.github.com>
2024-03-31 06:30:15 +0000

    커밋

--- backjoon.cpp
+++ backjoon.cpp
@@ -6,5 +6,26 @@ using namespace std;
 
 int main(void)
 {
+    int a,cnt=0;
     
+    cin>>a;
+    string k[a];
+    int check[a];
+    for(int i=0;i<a;i++)
+    {
+        cin>>k[i];
+        if(k[i][cnt]==0)
+        {
+            check[i]=cnt;
+            cnt=0;
+        }
+        else
+            cnt++;
+    }
+    for(int i=0;i<a;i++)
+    {
+        cout<<k[i][0]<<k[i][cnt]<<endl;
+    }
 }
+
+    
